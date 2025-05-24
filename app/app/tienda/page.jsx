7883f4c44f58e5b9b133d66d8789ad564df2494a
Isabel71btc <isabel.rvd71@gmail.com>
2025-05-24 23:29:59 +0200

    Crear página tienda

---
+++ app/app/tienda/page.jsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Bienvenida a la Tienda</h1>
+      <p>Muy pronto aquí podrás encontrar productos exclusivos relacionados con la Estrategia BTC 2025.</p>
+    </div>
+  );
+}
