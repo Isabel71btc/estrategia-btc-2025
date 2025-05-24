65d6e59f380876b3e437d91f0b75702e9bac6678
Isabel71btc <isabel.rvd71@gmail.com>
2025-05-24 19:46:30 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <head />
+      <head>
+        <title>Estrategia BTC 2025</title>
+      </head>
       <body>{children}</body>
     </html>
   );
