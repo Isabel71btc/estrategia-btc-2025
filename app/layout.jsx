bc72312b11f1224935c991354e34579d2ac6da9a
Isabel71btc <isabel.rvd71@gmail.com>
2025-05-24 20:53:32 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,11 @@
+export const metadata = {
+  title: 'Estrategia BTC 2025',
+  description: 'Estudio de criptomonedas',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <head>
-        <title>Estrategia BTC 2025</title>
-      </head>
       <body>{children}</body>
     </html>
   );
